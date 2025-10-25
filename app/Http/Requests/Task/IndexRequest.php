819facf71cd54356bddbfa976e58488125ebcc92
lifan2029 <lifan2029@gmail.com>
2025-10-25 10:58:19 +0500

    Added simple search for tasks

--- app/Http/Requests/Task/IndexRequest.php
+++ app/Http/Requests/Task/IndexRequest.php
@@ -14,6 +14,7 @@ class IndexRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'search' => ['sometimes', 'string', 'max:255'],
             'order_by' => ['sometimes', 'string', 'in:id,title,due_date,created_at'],
             'order_direction' => ['sometimes', 'string', 'in:asc,desc'],
             'page' => ['sometimes', 'integer', 'min:1'],
--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -3,12 +3,15 @@
 namespace App\Repositories;
 
 use App\Models\Task;
+use Illuminate\Database\Eloquent\Builder;
 
 class TaskRepository extends BaseRepository
 {
     public function getPaginated(array $params = [])
     {
         $query = Task::where('user_id', auth()->id());
+
+        $query = $this->applyFilter($query, $params);
 
         $query = $this->applyOrderBy($query, $params);
 
@@ -20,5 +23,17 @@ class TaskRepository extends BaseRepository
     public function store(array $data): Task
     {
         return Task::create($data);
+    }
+
+    private function applyFilter(Builder $query, array $params): Builder
+    {
+        if (isset($params['search'])) {
+            $query->where(function (Builder $q) use ($params) {
+                $q->where('title', 'like', '%' . $params['search'] . '%')
+                  ->orWhere('description', 'like', '%' . $params['search'] . '%');
+            });
+        }
+
+        return $query;
     }
 }
